1d318dbf145a6868b2ff0e7a84acc9b318025530
mickael hibon <hibon.mickael@gmail.com>
2026-03-03 15:57:21 +0100

    Wip Quest controller

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,10 +1,22 @@
 class QuestsController < ApplicationController
+  def show
+    @quest = Quest.find(params[:id])
+    @chat = @quest.chats.where(user: current_user)
+    @card = @chat.cards.last # ??
+  end
+
   def index
+    @quests = Quest.all
   end
 
   def create
-  end
-
-  def show
+    @quest = Quest.new
+    # @chat = Chat.new
+    # @card = Card.new
+    if @quest.save
+      redirect_to "/quests/:id"
+    else
+      render "quests", status: :unprocessable_entity
+    end
   end
 end
